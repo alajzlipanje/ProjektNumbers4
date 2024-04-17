bc6adf114186596f233f381236e4ababd755e0f4
alajzlipanje <aljaz.lipanje@gmail.com>
2024-04-17 18:00:13 +0200

    dwadawd

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { Input } from "postcss";
 import { useEffect, useState } from "react";
 
 export default function App() {
   const [fact, setFact] = useState({});
+  const [numbers, setNumber] = useState(0);
+  const [countries, setcountries] = useState([]);
+  const [colors, setColors] = useState([
+    "rdeča",
+    "rumena",
+    "modra",
+    "zelena",
+    "oranžna",
+  ]);
 
   async function getRandomFact() {
     const response = await fetch("http://numbersapi.com/random?json");
@@ -9,9 +19,28 @@ export default function App() {
     setFact(data);
   }
 
+  async function Countries() {
+    const country = await fetch(
+      "https://restcountries.com/v3.1/all?fields=name,flag,borders,region",
+    );
+    const country12 = await country.json();
+    setcountries(country12);
+  }
+
   useEffect(() => {
-    getRandomFact();
+    Countries();
   }, []);
 
-  return <p>{fact.text}</p>;
+  return (
+    <>
+      {countries
+        .filter((country) => country.region == "Europe")
+        .map((country) => (
+          <p>{country.name.common}</p>
+        ))}
+      {colors.map((color) => (
+        <p>{color}</p>
+      ))}
+    </>
+  );
 }
